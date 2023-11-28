fc9c4aee1385c80b3454592423f61ef4a2a86b21
Mrbilal <bilal_itchir@Outlook.fr>
2023-11-28 14:21:27 +0100

    🐛 corriger(index.js) : changer le port d'écoute de 3001 à 3000 🔧 refactor(index.js) : utiliser la méthode path.join pour construire les chemins de fichiers 🔧 refactor(index.js) : supprimer le code commenté inutilisé pour la suppression de films

--- index.js
+++ index.js
@@ -6,59 +6,36 @@
  */
 
 // ------------------------- IMPORTS ------------------------- //
-const express = require("express"); // Framework pour NodeJS
-const app = express(); // Création de l'application
-const PORT = 3001; // Définition du port d'écoute
-const Save = require("./functions/Save"); // Importation de la fonction Save
+const express = require("express"); 
+const app = express(); 
+const PORT = 3000; 
+const Save = require("./functions/Save"); 
 const cors = require('cors')
 const path = require('path')
 // const Delete = require("./functions/Delete"); // Importation de la fonction Delete
 
 // ------------------------- ROUTES ------------------------- //
 
-/**
- * Middleware
- * Le middleware est une fonction qui va être exécutée à chaque requête
- * elle va permettre de traiter les données avant de les envoyer au serveur
- * cela agit comme un filtre pour éviter les erreurs et définir des règles.
- * 
- * Dans notre cas, on va utiliser le middleware express.urlencoded qui va
- * nous permettre de récupérer les données envoyées par le formulaire.
- * 
- * Le paramètre extended à true permet de récupérer les données envoyées
- * par le formulaire sous la forme d'un objet.
- */
 app.use(express.urlencoded({ extended: true }),cors());
 app.use(express.json()) 
 app.use(express.static('./client/build'))
 
-
 // Route permettant de traiter l'enregistrement d'un film dans la liste des favoris
 app.post("/api/save", (req, res) => {
-  const imdbID = req.body.imdbID // On récupère les données envoyées par le formulaire
-  const saveStatus = Save(imdbID); // On appelle la fonction Save en lui envoyant les données
-  // Vérification du statut de la fonction Save
+  const imdbID = req.body.imdbID 
+  const saveStatus = Save(imdbID);
+
   if (saveStatus) {
     res.redirect('/')
   }
 });
 
 app.get("/api/fav", (_,res) => {
-  res.sendFile(__dirname + "/data.json")
+  res.sendFile(path.join(__dirname, "./data.json"))
 })
 
 app.get("/*", (req,res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"))
 })
-// Route permettant de traiter la suppression d'un film dans la liste des favoris
-// app.post("/api/delete", (req, res) => {
-//   const imdbID = req.body // On récupère les données envoyées par le formulaire
-//   Delete(imdbID); // On appelle la fonction Delete en lui envoyant les données
-// });
 
-
-/** Lancement du serveur
-* La méthode listen permet de lancer le serveur sur le port défini
-* dans la constante PORT
-*/
 app.listen(PORT, () => console.log("Le serveur est lancé sur le port " + PORT));
